41ad21337b1e46ef13f7d55298f48b3e11cec816
jjkidzxs <jjabendan1@gmail.com>
2024-05-24 20:17:35 +0800

    first

---
+++ src/zLandingpage/SplashScreen.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../zComponents/images/emsxcit.png';
+import './splashScreen.css';
+
+const AdminStudent = () => {
+  return (
+    <div className='AdminStudent__section'>
+      <div className='AdminStudent__container'>
+        <div>
+        <p className='logo-text'>EVENT MANAGEMENT SYSTEM</p>
+          <img className='EMSlogo' src={Logo} alt="CIT-U NSTP Logo" />
+        </div>
+        <div className='AdminStudent__btn'>
+          <Link to="/Student/Login">Login as Student</Link>
+          <Link to="/Teacher/Login">Login as Teacher</Link>
+        </div>
+        <Link className='adminlink' to="/Admin/Login">Switch to Admin</Link>
+      </div>
+    </div>
+  );
+};
+
+export default AdminStudent;
